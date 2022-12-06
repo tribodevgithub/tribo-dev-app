22758a2bda94d0ced92d1f44b0306c715e0dc20b
Bruno Santana <bsantana.it@gmail.com>
2022-12-06 19:36:08 -0300

    Update home.blade.php

--- resources/views/web/default/pages/home.blade.php
+++ resources/views/web/default/pages/home.blade.php
@@ -763,7 +763,7 @@
             <div class="container">
             <!-- Modal -->
             <div class="modal fade" id="modalVideo" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-              <div class="modal-dialog" role="document">
+              <div class="modal-dialog modal-xl" role="document">
                 <div class="modal-content">
                   <div class="modal-body">
                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
