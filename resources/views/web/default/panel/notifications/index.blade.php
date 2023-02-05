311b69b5314694a931179f8004e7811b0a628eda
Bruno Santana <bsantana.it@gmail.com>
2023-02-05 13:28:47 -0300

    Update index.blade.php

--- resources/views/web/default/panel/notifications/index.blade.php
+++ resources/views/web/default/panel/notifications/index.blade.php
@@ -1,7 +1,7 @@
 @extends(getTemplate() .'.panel.layouts.panel_layout')
 
-{{setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
-date_default_timezone_set('America/Sao_Paulo');}}
+{{setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese')}}
+{{date_default_timezone_set('America/Sao_Paulo');}}
 
 @section('content')
     <section>
