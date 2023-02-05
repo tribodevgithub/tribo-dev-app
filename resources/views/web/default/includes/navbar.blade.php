4c07a01a032979feed804b198fc067a7fc9ec559
Bruno Santana <bsantana.it@gmail.com>
2023-02-05 16:19:21 -0300

    navbar update

--- resources/views/web/default/includes/navbar.blade.php
+++ resources/views/web/default/includes/navbar.blade.php
@@ -101,7 +101,7 @@
                                 <a class="nav-link" href="/">Home</a>
                     </li>
                     <li class="nav-item">
-                                <a class="nav-link" href="/">Tribo Vip</a>
+                                <a class="nav-link" href="/">Assine</a>
                     </li>
                     <li class="nav-item">
                                 <a class="nav-link" href="/">A TriboDev</a>
