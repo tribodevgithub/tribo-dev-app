7c14a2795cad1642fc6f23b7e92266b7f38e3203
Bruno Santana <bsantana.it@gmail.com>
2023-02-06 12:59:13 -0300

    Update home.blade.php

--- resources/views/web/default/pages/home.blade.php
+++ resources/views/web/default/pages/home.blade.php
@@ -584,22 +584,12 @@
             </div>
         @endif
 
-        <div class="stats-container">
+    <div class="stats-container">
         <div class="container">
             <div class="text-center">
                 <h1>Escolha a jornada certa para sua carreira em tecnologia</h1>
             </div>
             <div class="row">
-                <div class="col-sm-6 col-lg-4 mt-25 mt-lg-0">
-                    <div class="stats-item d-flex flex-column align-items-center text-center py-30 px-5 w-100">
-                        <div class="stat-icon-box teacher">
-                            <img src="/assets/default/img/stats/teacher.svg" alt=""/>
-                        </div>
-                        <h4 class="stat-title">Comunidade Experiente</h4>
-                        <p class="stat-desc mt-10">Tenha acesso a uma comunidade de desenvolvedores experientes que compartilham conhecimentos, dicas e suporte.</p>
-                    </div>
-                </div>
-
                 <div class="col-sm-6 col-lg-4 mt-25 mt-lg-0">
                     <div class="stats-item d-flex flex-column align-items-center text-center py-30 px-5 w-100">
                         <div class="stat-icon-box teacher">
