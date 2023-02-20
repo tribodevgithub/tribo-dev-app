4af872de124410ed71c2b28c51f07c2497301e9b
Bruno Santana <bsantana.it@gmail.com>
2023-02-20 16:29:52 -0300

    Update home.blade.php

--- resources/views/web/default/pages/home.blade.php
+++ resources/views/web/default/pages/home.blade.php
@@ -477,8 +477,6 @@
                     </div>
                     <div class="position-relative mt-50">
                         <div class="swiper-container subscribes-swiper px-12">
-                            <div class="row">
-                                <div class="col-xs-12 ">
                                     <nav>
                                         <div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
                                             <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Tribo Free</a>
@@ -514,8 +512,6 @@
                                             </div>
                                         </div>
                                     </div>
-
-                                </div>
                             </div>
 
 
