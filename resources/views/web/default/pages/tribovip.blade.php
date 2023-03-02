b8edd8686a9dfdc0f4fef80279a04b8b7f97f150
Bruno Santana <bsantana.it@gmail.com>
2023-03-02 18:55:38 -0300

    Update tribovip.blade.php

--- resources/views/web/default/pages/tribovip.blade.php
+++ resources/views/web/default/pages/tribovip.blade.php
@@ -13,7 +13,7 @@
                     <div class="default-title">
                         <h1><span class="text-primary">Transforme sua carreira</span><br>Aprenda programação com cursos avançados e projetos reais.</h1>
                         <p class="mt-30 mb-30 text-gray">O <b class="text-primary">TriboVIP</b> é a escolha perfeita para quem deseja iniciar ou progredir na carreira de programação. Com acesso a cursos avançados, projetos reais e mentorias semanais, você terá a garantia de que está sempre aprendendo e evoluindo. Não perca mais tempo e junte-se ao TriboVIP agora mesmo para alcançar o sucesso em sua carreira de programação!</p>
-                        <a href="#" class="btn btn-primary rounded-pill">Assinar Agora!</a>
+                        <a href="#assinar" class="btn btn-primary rounded-pill">Assinar Agora!</a>
                     </div>
                 </div>
                 <div class="col-lg-6">
@@ -36,7 +36,7 @@
                         Assinando o <b class="text-primary">TriboVIP</b>, você terá acesso aos melhores treinamentos para prepará-lo de forma rápida e eficaz para vagas internacionais. Com nossos cursos, você aprenderá como criar um currículo internacional impressionante, atrair a atenção de empresas de todo o mundo e aprimorar seu inglês técnico para profissionais de tecnologia.
                     </p>
 
-                    <a href="#" type="button" class="btn btn-outline-primary btn-block mt-30">Assinar TriboVIP</a>
+                    <a href="#assinar" type="button" class="btn btn-outline-primary btn-block mt-30">Assinar TriboVIP</a>
                 </div>
             </div>
         </div>
@@ -310,7 +310,7 @@
         </div>
     </section>
 
-    <section>
+    <section id="assinar">
    <div class="container mt-50 mb-50 pt-50 pb-50">
       <div class="row">
          <div class="col-lg-7">
